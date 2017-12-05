03705e26b44251ff2ba4a4262401a699184e49b5
Coul33t <Coulis1990@gmail.com>
2017-12-05 15:47:09 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,54 @@
+var global = this;
+var stop_words = [];
+
+function import_stopwords(file) {
+    
+    var xhr = new XMLHttpRequest();
+    // false = synchronous
+    xhr.open('GET', file, false);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            global.stop_words = [];
+
+            for (line of xhr.responseText.split('\n'))
+                global.stop_words.push(line);
+
+        }
+    }
+
+    xhr.send();
+    
+    /*var r_data = [];
+
+    $.get(file, function(data) {
+        r_data = data;
+        console.log('r_data inside jquery: ' + r_data);
+    }, 'text');
+
+    console.log("r_data outside jquery: " + r_data);
+    return r_data;*/
+}
+
+function extract_keywords(sentence) {
+    import_stopwords('stopwords.txt');
+    sentence_array = sentence.toLowerCase();
+    sentence_array = sentence_array.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    console.log('after replace: ' + sentence_array);
+    var separators = [' ', '\\\''];
+    sentence_array = sentence_array.split(new RegExp(separators.join('|'), 'g'));
+    console.log('after split: ' + sentence_array);
+
+    for (word of global.stop_words) {
+        if (sentence_array.indexOf(word) !== -1) {
+            sentence_array.splice(sentence_array.indexOf(word), 1);
+        }
+    }
+
+    console.log('after custom filter: ' + sentence_array);
+    //console.log(global.stop_words);
+    //sentence_array = sentence_array.filter(word => !global.stop_words.includes(word));
+    //console.log('after filter: ' + sentence_array);
+}
+
+extract_keywords('Bonjour, je suis Quentin. J\'aime manger... Et dormir.')
