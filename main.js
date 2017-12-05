38af6bd1b88f20b7b96d94d4fdc409737cfeccc2
Coul33t <Coulis1990@gmail.com>
2017-12-05 17:57:46 +0100

    Stop-words removing

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 var global = this;
+// This array contains the stop-words, i.e. words that are
+// useless to understand the request (see stopwords.txt)
 var stop_words = [];
+
+// This array contains the verbs that help us understanding
+// what the user wants (see verbs.txt)
+var verbs = [];
+
+// This array contains the nouns that help us understanding
+// what the user wants (see verbs.txt)
+var nouns = [];
+
+// Use to have verbose output on the JS console
+var DEBUG = false;
 
 function import_stopwords(file) {
     
     var xhr = new XMLHttpRequest();
-    // false = synchronous
+    // false = synchronous (ugly)
+    // TODO: better
     xhr.open('GET', file, false);
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             global.stop_words = [];
+            console.log(xhr.responseText);
 
-            for (line of xhr.responseText.split('\n'))
+            for (line of xhr.responseText.split('\n')) {
                 global.stop_words.push(line);
+            }
 
         }
     }
@@ -32,29 +48,53 @@ function import_stopwords(file) {
 
 function extract_keywords(sentence) {
     import_stopwords('stopwords.txt');
+    // We send it to lower case
     sentence_array = sentence.toLowerCase();
 
+    // We strip off every punctuation
     sentence_array = sentence_array.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-    console.log('after replace: ' + sentence_array);
+    
+    if (DEBUG)
+        console.log('after replace: ' + sentence_array);
 
+    // We also separate words with a ', such as in " J'aime "
     var separators = [' ', '\\\''];
     sentence_array = sentence_array.split(new RegExp(separators.join('|'), 'g'));
-    console.log('after split: ' + sentence_array);
+
+    if (DEBUG)
+        console.log('after split: ' + sentence_array);
+
+    // We get the indexes of stop-words
+    idx_to_splice = [];
 
     for (word of global.stop_words) {
-        console.log('type of word: ' + typeof(word) + ' (' + word + ')');
-        console.log('type of sentence array: ' + typeof(sentence_array[1]) + ' (' + sentence_array[1] + ')');
-        console.log(word.toString() === sentence_array[1].toString());
-        console.log(sentence_array.indexOf(word));
-        if (sentence_array.indexOf(word) !== -1) {
-            sentence_array.splice(sentence_array.indexOf(word), 1);
+        // Yo this .trim() fucker is what I needed
+        if (sentence_array.indexOf(word.trim()) !== -1) {
+            idx_to_splice.push(sentence_array.indexOf(word.trim()));        
         }
     }
 
-    console.log('after custom filter: ' + sentence_array);
-    //console.log(global.stop_words);
-    //sentence_array = sentence_array.filter(word => !global.stop_words.includes(word));
-    //console.log('after filter: ' + sentence_array);
+    // We sort them in descending order, because we can then
+    // safely delete the entries from the end to the beginning
+    // (it won't mess with the indexes values)
+    idx_to_splice.sort().reverse();
+
+    if (DEBUG)
+        console.log(idx_to_splice);
+
+    // We delete the stop words
+    for (idx of idx_to_splice) {
+        sentence_array.splice(idx, 1);
+    }
+
+    if (DEBUG)
+        console.log('after custom filter: ' + sentence_array);
 }
 
-extract_keywords('Bonjour, je suis Quentin. J\'aime manger... Et dormir.')
+$(document).ready(function() {
+    $('#sentence_button').click(function() {
+        extract_keywords($('#form_sentence_value').val());
+    });
+});
+
+//extract_keywords('Bonjour, je suis Quentin. J\'aime manger... Et dormir.')
